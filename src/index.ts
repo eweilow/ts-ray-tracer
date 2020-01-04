72a42c2ee105021efc9aed6c9af47dc5f6853491
E. Weilow <eweilow@digitalkoppar.se>
2020-01-04 11:50:54 +0100

    Tweak startup

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import { render } from "react-dom";
 import { App } from "./app";
 import { createElement } from "react";
 
-let el = document.querySelector("#react");
-if (el == null) {
-  el = document.createElement("div");
-  el.id = "react";
-  document.body.appendChild(el);
+let el: any;
+function start() {
+  el = document.querySelector("#react");
+  if (el == null) {
+    el = document.createElement("div");
+    el.id = "react";
+    document.body.appendChild(el);
+  }
+  render(createElement(App), el);
 }
-render(createElement(App), el);
+
+if (document?.readyState === "complete") {
+  start();
+} else {
+  window.addEventListener("load", () => {
+    start();
+  });
+}
